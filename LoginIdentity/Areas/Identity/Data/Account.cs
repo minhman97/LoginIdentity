33b51fb75bea8ba6ffa2fb4a2015bec9f7dac6f5
minh man <minhman.carter@gmail.com>
2022-05-18 22:48:40 +0700

    add font awesome and modify login page

---
+++ LoginIdentity/Areas/Identity/Data/Account.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginIdentity.Data;
+
+public class Account
+{
+    [Key]
+    public int Id { get; set; }
+    [MaxLength(150)]
+    public string FirstName { get; set; }
+    [MaxLength(150)]
+    public string LastName { get; set; }
+}
